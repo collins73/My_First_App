d110ae9ae19061cc3a05866efdb20fcdb54b6de9
Demayne Collins <Collinsd73@gmail.com>
2020-09-07 10:38:16 -0400

    Add Toast method to display a message

--- app/src/main/java/com/myapp/exercise1/MainActivity.kt
+++ app/src/main/java/com/myapp/exercise1/MainActivity.kt
@@ -2,10 +2,15 @@ package com.myapp.exercise1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Hello from Android!", Toast.LENGTH_LONG).show()
+
     }
 }
